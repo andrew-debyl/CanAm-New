16741bf52b14716e41abb6eb53c3cbe2f40aebba
Andrew <andrewdebyl@gmail.com>
2024-10-29 17:30:51 -0400

    more

--- pages/parts.js
+++ pages/parts.js
@@ -7,35 +7,35 @@ import Head from "next/head";
 const Parts = () => {
   function addPartsJsonLd() {
     return {
-      __html: `
-      "@context": "https://schema.org",
-      "@type": "WebPage",
-      "name": "Parts",
-      "description": "Explore CanAm Imaging's extensive range of genuine Xerox and Kodak Digimaster parts, available at competitive prices.",
-      "url": "https://can-am-new.vercel.app/parts", // Replace with your actual URL
-      "mainEntity": {
-        "@type": "Organization",
-        "name": "CanAm Imaging",
-        "url": "https://can-am-new.vercel.app/" // Replace with your actual URL
-      },
-      "breadcrumb": {
-        "@type": "BreadcrumbList",
-        "itemListElement": [
-          {
-            "@type": "ListItem",
-            "position": 1,
-            "name": "Home",
-            "item": "https://can-am-new.vercel.app/" // Replace with your actual URL
-          },
-          {
-            "@type": "ListItem",
-            "position": 2,
-            "name": "Parts",
-            "item": "https://can-am-new.vercel.app/parts" // Replace with your actual URL
-          }
-        ]
-      }
-  `,
+      __html: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": "Parts",
+        "description": "Explore CanAm Imaging's extensive range of genuine Xerox and Kodak Digimaster parts, available at competitive prices.",
+        "url": "https://can-am-new.vercel.app/parts",
+        "mainEntity": {
+          "@type": "Organization",
+          "name": "CanAm Imaging",
+          "url": "https://can-am-new.vercel.app/"
+        },
+        "breadcrumb": {
+          "@type": "BreadcrumbList",
+          "itemListElement": [
+            {
+              "@type": "ListItem",
+              "position": 1,
+              "name": "Home",
+              "item": "https://can-am-new.vercel.app/"
+            },
+            {
+              "@type": "ListItem",
+              "position": 2,
+              "name": "Parts",
+              "item": "https://can-am-new.vercel.app/parts"
+            }
+          ]
+        }
+      }),
     };
   }
   return (
